c6cf9782ed890ee095802990f722387977bd4a01
Robin Gower <robin.git@infonomics.ltd.uk>
2016-07-26 16:23:08 +0200

    Tweak sed call in cache buster
    Previous line failed for me as it was trying to find a file
    corresponding to the regex substitution.
    
    I'm using GNU sed 4.2.2. YMMV.
    
    We might need to make the argument an expression explicitly with `-e`.

--- bust-cache.sh
+++ bust-cache.sh
@@ -3,4 +3,4 @@
 echo "Busting cache ..."
 
 GIT_REF=$(git rev-parse --short HEAD)
-grep -lR "_version" src resources | xargs sed -i '' s/_version=\.\*\"/_version=${GIT_REF}\"/g
+grep -lR "_version" src resources | xargs sed -i s/_version=\.\*\"/_version=${GIT_REF}\"/g
